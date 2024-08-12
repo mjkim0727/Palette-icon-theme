bd4bf965aa74e98a850f0e29289c810d84dc0360
주르르 <78864678+mjkim0727@users.noreply.github.com>
2024-08-12 20:25:38 +0900

    added icons, gruvbox color add

---
+++ alternatives/Gruvbox-Color/gruvbox-folder-color-16.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sed -i 's/#4f4f4f/#32302f/g' *-black*.svg;
+sed -i 's/#3a3a3a/#282828/g' *-black*.svg;
+sed -i 's/#1f6ca9/#49665b/g' *-blue*.svg;
+sed -i 's/#2196f3/#83a598/g' *-blue*.svg;
+sed -i 's/#746056/#665c54/g' *-brown*.svg;
+sed -i 's/#a0887e/#7c6f64/g' *-brown*.svg;
+sed -i 's/#00bcd4/#58a7aa/g' *-cyan*.svg;
+sed -i 's/#007d8d/#458588/g' *-cyan*.svg;
+sed -i 's/#5e872d/#98971a/g' *-green*.svg;
+sed -i 's/#8bc34a/#b8bb26/g' *-green*.svg;
+sed -i 's/#6d6d6d/#7c6f64/g' *-grey*.svg;
+sed -i 's/#9e9e9e/#928374/g' *-grey*.svg;
+sed -i 's/#a36000/#d65d0e/g' *-orange*.svg;
+sed -i 's/#ff9800/#fe8019/g' *-orange*.svg;
+sed -i 's/#a7224e/#c15071/g' *-pink*.svg;
+sed -i 's/#f06292/#d3869b/g' *-pink*.svg;
+sed -i 's/#7e57c2/#b16286/g' *-purple*.svg;
+sed -i 's/#503188/#8f3f71/g' *-purple*.svg;
+sed -i 's/#9a2623/#cc241d/g' *-red*.svg;
+sed -i 's/#ef5350/#fb4934/g' *-red*.svg;
+sed -i 's/#c28c04/#d79921/g' *-yellow*.svg;
+sed -i 's/#fbc02d/#fabd2f/g' *-yellow*.svg;
