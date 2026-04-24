4e56eaec92eed1429622bd8dcbdc10677288ad96
마리임 <78864678+mjkim0727@users.noreply.github.com>
2026-04-25 02:34:41 +0900

    (WIP) Adding KDE ColorScheme Folders

---
+++ alternatives/KDE-ColorScheme/kde-folder-color.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sed -i 's/#4f4f4f/#333333/g' *-black*.svg;
+sed -i 's/#3a3a3a/#222222/g' *-black*.svg;
+sed -i 's/#1f6ca9/#1e5090/g' *-blue*.svg;
+sed -i 's/#2196f3/#4183d7/g' *-blue*.svg;
+sed -i 's/#746056/#573c24/g' *-brown*.svg;
+sed -i 's/#a0887e/#8b6039/g' *-brown*.svg;
+sed -i 's/#00bcd4/#21bbd7/g' *-cyan*.svg;
+sed -i 's/#007d8d/#18869a/g' *-cyan*.svg;
+sed -i 's/#5e872d/#2d815d/g' *-green*.svg;
+sed -i 's/#8bc34a/#3bad7e/g' *-green*.svg;
+sed -i 's/#6d6d6d/#667078/g' *-grey*.svg;
+sed -i 's/#9e9e9e/#a7afb4/g' *-grey*.svg;
+sed -i 's/#a36000/#ab6604/g' *-orange*.svg;
+sed -i 's/#ff9800/#f89406/g' *-orange*.svg;
+sed -i 's/#a7224e/#9d294b/g' *-pink*.svg;
+sed -i 's/#f06292/#d86687/g' *-pink*.svg;
+sed -i 's/purple/violet/g' *-violet*.svg;
+sed -i 's/#7e57c2/#8e44ad/g' *-violet*.svg;
+sed -i 's/#503188/#65307b/g' *-violet*.svg;
+sed -i 's/#9a2623/#9f072d/g' *-red*.svg;
+sed -i 's/#ef5350/#eb0a42/g' *-red*.svg;
+sed -i 's/#c28c04/#b8920c/g' *-yellow*.svg;
+sed -i 's/#fbc02d/#f2cb40/g' *-yellow*.svg;
